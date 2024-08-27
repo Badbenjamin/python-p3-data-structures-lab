587a3b7fe6ab6a2a961362866b482ff7dc58e695
Ben McCarthy <bmccarthy90@gmail.com>
2024-08-26 21:12:23 -0400

    first three tests passing

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,13 +17,32 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    spicy_list = []
+    for food in spicy_foods:
+        spicy_list.append(food["name"])
+    return spicy_list
+       
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_list = []
+    for food in spicy_foods:
+        # print(food["heat_level"])
+        if food["heat_level"] > 5:
+            spiciest_list.append(food)
+        else:
+            pass
+    return spiciest_list
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = int(food["heat_level"]) * "🌶"
+        print(f"{name} ({cuisine}) | Heat Level: {heat_level}")
+        # print(food["name"])
+        # print(food["cuisine"])
+        # print(food["heat_level"])
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
@@ -36,3 +55,5 @@ def get_average_heat_level(spicy_foods):
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
+
+print_spicy_foods(spicy_foods)
